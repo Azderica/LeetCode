72183693916b00d149768094e0a3d966492c825f
Myeonghun Park <42582516+Azderica@users.noreply.github.com>
2022-02-25 18:56:27 -0800

    Time: 7 ms (100.00%), Space: 42.1 MB (87.80%) - LeetHub

---
+++ 847-shortest-path-visiting-all-nodes/847-shortest-path-visiting-all-nodes.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int shortestPathLength(int[][] graph) {
+        int n = graph.length;
+        if(n == 1) return 0;
+
+        int endingMask = (1 << n) - 1;
+        boolean[][] visited = new boolean[n][endingMask];
+        ArrayList<int[]> queue = new ArrayList<>();
+
+        for(int i = 0; i < n; i++) {
+            queue.add(new int[] {i, 1 << i});
+            visited[i][1 << i] = true;
+        }
+
+        int steps = 0;
+        while(!queue.isEmpty()) {
+            ArrayList<int[]> nextQueue = new ArrayList<>();
+            for(int i = 0; i < queue.size(); i++) {
+                int[] currentPair = queue.get(i);
+                int node = currentPair[0];
+                int mask = currentPair[1];
+                for(int neighbor : graph[node]) {
+                    int nextMask = mask | (1 << neighbor);
+                    if(nextMask == endingMask) {
+                        return 1 + steps;
+                    }
+
+                    if(!visited[neighbor][nextMask]) {
+                        visited[neighbor][nextMask] = true;
+                        nextQueue.add(new int[] {neighbor, nextMask});
+                    }
+                }
+            }
+            steps++;
+            queue = nextQueue;
+        }
+
+        return -1;
+    }
+
+}
