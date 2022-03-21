2af611398547638d754fd1adf829d19d00d4a21d
Myeonghun Park <42582516+Azderica@users.noreply.github.com>
2022-03-21 04:09:01 -0700

    Time: 1 ms (99.51%), Space: 42.2 MB (83.66%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
@@ -1,6 +1,45 @@
+
+
+import java.util.Random;
+
 class Solution {
+    int[] nums;
+
     public int findKthLargest(int[] nums, int k) {
-        Arrays.sort(nums);
-        return nums[nums.length - k];
+        this.nums = nums;
+        int size = nums.length;
+        return quickSelect(0, size - 1, size - k);
+    }
+
+    private int quickSelect(int left, int right, int kSmallest) {
+        if(left == right) return this.nums[left];
+        Random randomNum = new Random();
+        int pivotIndex = left + randomNum.nextInt(right - left);
+        pivotIndex = partition(left, right, pivotIndex);
+        if(kSmallest == pivotIndex) return this.nums[kSmallest];
+        else if(kSmallest < pivotIndex) return quickSelect(left, pivotIndex - 1, kSmallest);
+        else return quickSelect(pivotIndex + 1, right, kSmallest);
+    }
+
+    private int partition(int left, int right, int pivotIndex) {
+        int pivot = this.nums[pivotIndex];
+        swap(pivotIndex, right);
+        int storeIndex = left;
+
+        for(int i = left; i <= right; i++) {
+            if(this.nums[i] < pivot) {
+                swap(storeIndex, i);
+                storeIndex++;
+            }
+        }
+
+        swap(storeIndex, right);
+        return storeIndex;
+    }
+
+    public void swap(int a, int b) {
+        int tmp = this.nums[a];
+        this.nums[a] = this.nums[b];
+        this.nums[b] = tmp;
     }
 }
