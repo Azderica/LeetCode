0b79a7c6e127008c0bcaca2a2cf99c1c18269d88
Myeonghun Park <42582516+Azderica@users.noreply.github.com>
2022-05-28 20:23:58 +0900

    Time: 109 ms (19.20%), Space: 97.1 MB (25.79%) - LeetHub

---
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maximumBags(int[] capacity, int[] rocks, int additionalRocks) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < rocks.length; i++) {
+            list.add(capacity[i] - rocks[i]);
+        }
+        Collections.sort(list);
+        int ans = 0, count = 0;
+        for(int value : list) {
+            count += value;
+            if(count > additionalRocks) break;
+            ans++;
+        }
+        return ans;
+    }
+}
